eea8f6d0f4990474ce85589629337dfb75f4962b
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-06 14:16:26 +0800

    Update auto_choujiang_draw.rb

--- jobs/auto_choujiang_draw.rb
+++ jobs/auto_choujiang_draw.rb
@@ -18,11 +18,11 @@ module ::Jobs
         next unless info[:draw_time] && Time.now >= info[:draw_time]
         # 判断是否已经开奖过
         next if topic.tags.exists?(name: "choujiang_drawn")
-        
+
         winners = ::Choujiang.select_winners(topic, info)
         Rails.logger.warn("【Choujiang调试】主题ID:#{topic.id} 抽中的用户:#{winners.inspect}")
         ::Choujiang.announce_winners(topic, winners, info)
-        
+
         # 正确添加Tag对象
         tag = Tag.find_or_create_by(name: "choujiang_drawn")
         unless topic.tags.include?(tag)
