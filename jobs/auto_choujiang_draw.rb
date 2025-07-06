2e760ce510b2a85a86599d810969385b27b93a12
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-06 12:55:59 +0800

    Update auto_choujiang_draw.rb

--- jobs/auto_choujiang_draw.rb
+++ jobs/auto_choujiang_draw.rb
@@ -7,12 +7,16 @@ module ::Jobs
     def execute(args)
       return unless SiteSetting.choujiang_enabled?
 
-      ::Choujiang.choujiang_topics.each do |topic|
+      topics = ::Choujiang.choujiang_topics
+      Rails.logger.warn("【Choujiang调试】找到了#{topics.count}个抽奖主题")
+      topics.each do |topic|
         first_post = topic.first_post
         info = ::Choujiang.parse_choujiang_info(first_post)
+        Rails.logger.warn("【Choujiang调试】主题ID:#{topic.id} info:#{info.inspect} now:#{Time.now}")
         next unless info[:draw_time] && Time.now >= info[:draw_time]
         next if topic.tags.include?("choujiang_drawn")
         winners = ::Choujiang.select_winners(topic, info)
+        Rails.logger.warn("【Choujiang调试】主题ID:#{topic.id} 抽中的用户:#{winners.inspect}")
         ::Choujiang.announce_winners(topic, winners, info)
         topic.tags << "choujiang_drawn"
         topic.save
