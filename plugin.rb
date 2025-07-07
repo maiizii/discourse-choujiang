54e935238b0b96a91af9ac58e5c9fa1996abb42c
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-07 17:34:34 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,12 @@
 # name: discourse-choujiang
 # about: 定时自动开奖的抽奖（choujiang）插件，支持自定义规则与自动开奖
-# version: 0.1
+# version: 0.2
 # authors: macgow
 # url: https://github.com/macgowge/discourse-choujiang
+
+register_asset "javascripts/discourse/initializers/add-choujiang-template-button.js"
+register_asset "javascripts/discourse/components/choujiang-template-modal.js"
+register_asset "javascripts/discourse/templates/components/choujiang-template-modal.hbs"
 
 enabled_site_setting :choujiang_enabled
 
