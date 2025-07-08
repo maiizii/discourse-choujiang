28de35a1378c2728ae1d2443358b92c8d39a0b60
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 00:29:46 +0800

    Create choujiang_validator.rb

---
+++ lib/choujiang_validator.rb
@@ -0,0 +1,52 @@
+module ::ChoujiangValidator
+  REQUIRED_FIELDS = {
+    title:    /抽奖名称[:：]\s*(.+)/,
+    prize:    /奖品[:：]\s*(.+)/,
+    winners:  /获奖人数[:：]\s*(\d+)/,
+    draw_time:/开奖时间[:：]\s*([0-9\- :]+)/
+  }
+
+  def self.parse_and_validate(raw)
+    info = {}
+    errors = []
+
+    REQUIRED_FIELDS.each do |field, regex|
+      val = raw[regex, 1]&.strip
+      info[field] = field == :winners ? val&.to_i : val
+      errors << "#{field_label(field)}不能为空" unless val.present?
+    end
+
+    # 校验获奖人数
+    if info[:winners] && info[:winners].to_i <= 0
+      errors << "获奖人数必须为正整数"
+    end
+
+    # 校验开奖时间
+    if info[:draw_time]
+      begin
+        info[:draw_time] = ActiveSupport::TimeZone['Beijing'].parse(info[:draw_time]).utc
+        errors << "开奖时间必须是将来时间" if info[:draw_time] <= Time.now
+      rescue
+        begin
+          info[:draw_time] = Time.parse(info[:draw_time]).utc
+          errors << "开奖时间必须是将来时间" if info[:draw_time] <= Time.now
+        rescue
+          info[:draw_time] = nil
+          errors << "开奖时间格式无效"
+        end
+      end
+    end
+
+    [errors, info]
+  end
+
+  def self.field_label(key)
+    case key
+    when :title then "抽奖名称"
+    when :prize then "奖品"
+    when :winners then "获奖人数"
+    when :draw_time then "开奖时间"
+    else key.to_s
+    end
+  end
+end
