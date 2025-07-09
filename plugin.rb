6bb8ba0a43f7c3d3bd32cd199911652b47bd15a0
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 16:13:48 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,27 +6,30 @@
 
 enabled_site_setting :choujiang_enabled
 
+Rails.logger.warn("choujiang plugin loaded at top-level!")
+
 after_initialize do
   require_relative 'lib/choujiang'
   require_relative 'jobs/auto_choujiang_draw.rb'
 
-  # 测试 after_initialize 是否执行
-  Rails.logger.warn("choujiang plugin after_initialize called!")
+  Rails.logger.warn("choujiang after_initialize!")
 
-  # Discourse 3.x 官方推荐内容校验方法
-  register_post_custom_validator("choujiang_validator") do |raw, topic, user|
-    Rails.logger.warn("choujiang custom validator called!")
-    # 只校验主题首贴且有“抽奖活动”标签
-    if topic && topic.first_post_id == topic.id
-      tags = topic.tags.map(&:name) rescue []
-      if tags.include?("抽奖活动")
-        errors = []
-        errors << "缺少抽奖名称" unless raw.match?(/抽奖名称[:：]\s*.+/)
-        errors << "缺少奖品" unless raw.match?(/奖品[:：]\s*.+/)
-        errors << "缺少获奖人数" unless raw.match?(/获奖人数[:：]\s*\d+/)
-        errors << "缺少开奖时间" unless raw.match?(/开奖时间[:：]\s*[\d\- :]+/)
-        errors # 返回 errors 数组，内容会显示在前端
+  module ::ChoujiangPostCreatorPatch
+    def validate
+      Rails.logger.warn("choujiang postcreator validate called!")
+      super
+      # 只校验创建主题首贴时
+      if @opts[:topic_id].blank? && @opts[:raw].present? && @opts[:tags].present?
+        tags = Array(@opts[:tags]).map(&:to_s)
+        if tags.include?("抽奖活动")
+          @errors.add(:base, "缺少抽奖名称") unless @opts[:raw].match?(/抽奖名称[:：]\s*.+/)
+          @errors.add(:base, "缺少奖品") unless @opts[:raw].match?(/奖品[:：]\s*.+/)
+          @errors.add(:base, "缺少获奖人数") unless @opts[:raw].match?(/获奖人数[:：]\s*\d+/)
+          @errors.add(:base, "缺少开奖时间") unless @opts[:raw].match?(/开奖时间[:：]\s*[\d\- :]+/)
+        end
       end
     end
   end
+
+  ::PostCreator.prepend(::ChoujiangPostCreatorPatch)
 end
