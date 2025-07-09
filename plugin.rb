848c42c1151c4d70b3dae1745ef85391fb8cc77e
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 22:20:40 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,4 +9,12 @@ enabled_site_setting :choujiang_enabled
 after_initialize do
   require_relative 'lib/choujiang'
   require_relative 'jobs/auto_choujiang_draw.rb'
+
+  # 新建抽奖主题自动进入待审核，主贴不可编辑
+  DiscourseEvent.on(:post_created) do |post, opts, user|
+    if post.post_number == 1 && post.topic.tags.pluck(:name).include?("抽奖活动")
+      post.topic.update!(visible: false) # 需审核
+      post.update!(locked_by_id: Discourse.system_user.id, locked_at: Time.now) # 主贴不可编辑
+    end
+  end
 end
