76d8f07b9d32d5cbb061adaf5777c0ca5d1e9bfc
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 15:08:47 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -10,24 +10,37 @@ after_initialize do
   require_relative 'lib/choujiang'
   require_relative 'jobs/auto_choujiang_draw.rb'
 
+  # ------- 关键：自定义 PostValidator -------
   class ::ChoujiangPostValidator
-    def self.valid?(post)
-      return true unless post.post_number == 1
-      tags = (post.respond_to?(:tags) && post.tags.presence) || (post.topic&.tags&.map(&:name) || [])
-      return true unless tags.include?("抽奖活动")
-      errors = []
-      errors << "缺少抽奖名称" unless post.raw.match?(/抽奖名称[:：]\s*.+/)
-      errors << "缺少奖品" unless post.raw.match?(/奖品[:：]\s*.+/)
-      errors << "缺少获奖人数" unless post.raw.match?(/获奖人数[:：]\s*\d+/)
-      errors << "缺少开奖时间" unless post.raw.match?(/开奖时间[:：]\s*[\d\- :]+/)
-      errors.each { |e| post.errors.add(:base, e) }
-      errors.empty?
+    def initialize(post)
+      @post = post
+    end
+
+    def validate(errors)
+      # 只校验主题首贴
+      return unless @post.post_number == 1
+
+      # 获取标签名称
+      tags = if @post.respond_to?(:tags) && @post.tags.present?
+        @post.tags
+      elsif @post.topic && @post.topic.respond_to?(:tags)
+        @post.topic.tags.map(&:name)
+      else
+        []
+      end
+
+      # 只对含“抽奖活动”标签的主题校验（你可以根据实际标签名修改）
+      return unless tags.include?("抽奖活动")
+
+      errors.add(:base, "缺少抽奖名称") unless @post.raw.match?(/抽奖名称[:：]\s*.+/)
+      errors.add(:base, "缺少奖品") unless @post.raw.match?(/奖品[:：]\s*.+/)
+      errors.add(:base, "缺少获奖人数") unless @post.raw.match?(/获奖人数[:：]\s*\d+/)
+      errors.add(:base, "缺少开奖时间") unless @post.raw.match?(/开奖时间[:：]\s*[\d\- :]+/)
     end
   end
 
-  # 注册到Discourse的PostValidator链
-  validate_post_custom = lambda do |post|
-    ::ChoujiangPostValidator.valid?(post)
+  # 注册到 Discourse 的 PostValidator 链
+  add_to_class(:post_validator, :validate_choujiang_post) do
+    ::ChoujiangPostValidator.new(self.post).validate(self.errors)
   end
-  DiscourseEvent.on(:validate_post, &validate_post_custom)
 end
