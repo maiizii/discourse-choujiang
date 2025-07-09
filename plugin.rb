5db3fdfa8f7cba66f443e4e7e621eb4a7870d4a7
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 11:35:21 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -9,4 +9,21 @@ enabled_site_setting :choujiang_enabled
 after_initialize do
   require_relative 'lib/choujiang'
   require_relative 'jobs/auto_choujiang_draw.rb'
+
+  # 新增：发帖时内容格式校验
+  on(:validate_post) do |post|
+    next unless post.post_number == 1
+    tags = (post.respond_to?(:tags) && post.tags.presence) || (post.topic&.tags&.map(&:name) || [])
+    next unless tags.include?("抽奖活动")
+    # 校验四个字段
+    required = [
+      [/抽奖名称[:：]\s*.+/, "缺少抽奖名称"],
+      [/奖品[:：]\s*.+/, "缺少奖品"],
+      [/获奖人数[:：]\s*\d+/, "缺少获奖人数"],
+      [/开奖时间[:：]\s*[\d\- :]+/, "缺少开奖时间"]
+    ]
+    required.each do |regex, errmsg|
+      post.errors.add(:base, errmsg) unless post.raw.match?(regex)
+    end
+  end
 end
