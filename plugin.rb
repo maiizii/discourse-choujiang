b520b463b1ff2b2da0aacd9f14f4e80aad7b0667
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 16:42:01 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -14,12 +14,13 @@ after_initialize do
 
   module ::ChoujiangPostCreatorPatch
     def validate
-      Rails.logger.warn("choujiang postcreator validate called!")
+      Rails.logger.warn("choujiang postcreator validate called! opts: #{(@opts || {}).inspect}")
       super
-      # 只校验创建主题首贴时
       if @opts[:topic_id].blank? && @opts[:raw].present? && @opts[:tags].present?
+        Rails.logger.warn("choujiang postcreator validate: checking tags: #{@opts[:tags].inspect}")
         tags = Array(@opts[:tags]).map(&:to_s)
         if tags.include?("抽奖活动")
+          Rails.logger.warn("choujiang postcreator validate: in choujiang tag branch!")
           @errors.add(:base, "缺少抽奖名称") unless @opts[:raw].match?(/抽奖名称[:：]\s*.+/)
           @errors.add(:base, "缺少奖品") unless @opts[:raw].match?(/奖品[:：]\s*.+/)
           @errors.add(:base, "缺少获奖人数") unless @opts[:raw].match?(/获奖人数[:：]\s*\d+/)
