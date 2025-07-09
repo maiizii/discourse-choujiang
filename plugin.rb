83b6a3048667f3378e65611eb9e08f3199ce2698
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 17:07:26 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-choujiang
 # about: 定时自动开奖的抽奖（choujiang）插件，支持自定义规则与自动开奖
-# version: 0.2
+# version: 0.3
 # authors: macgow
 # url: https://github.com/macgowge/discourse-choujiang
 
@@ -12,23 +12,35 @@ after_initialize do
 
   Rails.logger.warn("choujiang after_initialize!")
 
-  module ::ChoujiangPostCreatorPatch
-    def validate
-      Rails.logger.warn("choujiang postcreator validate called! opts: #{(@opts || {}).inspect}")
-      super
-      if @opts[:topic_id].blank? && @opts[:raw].present? && @opts[:tags].present?
-        Rails.logger.warn("choujiang postcreator validate: checking tags: #{@opts[:tags].inspect}")
-        tags = Array(@opts[:tags]).map(&:to_s)
-        if tags.include?("抽奖活动")
-          Rails.logger.warn("choujiang postcreator validate: in choujiang tag branch!")
-          @errors.add(:base, "缺少抽奖名称") unless @opts[:raw].match?(/抽奖名称[:：]\s*.+/)
-          @errors.add(:base, "缺少奖品") unless @opts[:raw].match?(/奖品[:：]\s*.+/)
-          @errors.add(:base, "缺少获奖人数") unless @opts[:raw].match?(/获奖人数[:：]\s*\d+/)
-          @errors.add(:base, "缺少开奖时间") unless @opts[:raw].match?(/开奖时间[:：]\s*[\d\- :]+/)
-        end
-      end
+  # 只对主题首帖做内容强校验
+  module ::ChoujiangPostValidation
+    extend ActiveSupport::Concern
+
+    included do
+      validate :choujiang_topic_format, if: :is_choujiang_topic?
+    end
+
+    def is_choujiang_topic?
+      # 只校验主题首帖
+      is_first_post = self.post_number == 1
+      tags = self.topic&.tags&.map(&:name) || []
+      is_choujiang = tags.include?("抽奖活动")
+      Rails.logger.warn("choujiang post validate hook: is_first_post=#{is_first_post}, tags=#{tags.inspect}, is_choujiang=#{is_choujiang}")
+      is_first_post && is_choujiang
+    end
+
+    def choujiang_topic_format
+      Rails.logger.warn("choujiang post validate: raw=#{self.raw.inspect}")
+      errors.add(:base, "缺少抽奖名称") unless self.raw =~ /抽奖名称[:：]\s*.+/
+      errors.add(:base, "缺少奖品") unless self.raw =~ /奖品[:：]\s*.+/
+      errors.add(:base, "缺少获奖人数") unless self.raw =~ /获奖人数[:：]\s*\d+/
+      errors.add(:base, "缺少开奖时间") unless self.raw =~ /开奖时间[:：]\s*[\d\- :]+/
     end
   end
 
-  ::PostCreator.prepend(::ChoujiangPostCreatorPatch)
+  # 只 prepend 一次，防止重复
+  unless Post.included_modules.include?(::ChoujiangPostValidation)
+    Post.prepend(::ChoujiangPostValidation)
+    Rails.logger.warn("choujiang post validation prepended!")
+  end
 end
