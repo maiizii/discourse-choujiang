f511572757db7d481c26809d201e581a0071a3dd
macgowge <59905570+macgowge@users.noreply.github.com>
2025-07-09 17:41:49 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -12,34 +12,29 @@ after_initialize do
 
   Rails.logger.warn("choujiang after_initialize!")
 
-  # 只对主题首帖做内容强校验
   module ::ChoujiangPostValidation
-    extend ActiveSupport::Concern
-
-    included do
-      validate :choujiang_topic_format, if: :is_choujiang_topic?
-    end
-
-    def is_choujiang_topic?
-      # 只校验主题首帖
-      is_first_post = self.post_number == 1
-      tags = self.topic&.tags&.map(&:name) || []
-      is_choujiang = tags.include?("抽奖活动")
-      Rails.logger.warn("choujiang post validate hook: is_first_post=#{is_first_post}, tags=#{tags.inspect}, is_choujiang=#{is_choujiang}")
-      is_first_post && is_choujiang
-    end
-
-    def choujiang_topic_format
-      Rails.logger.warn("choujiang post validate: raw=#{self.raw.inspect}")
-      errors.add(:base, "缺少抽奖名称") unless self.raw =~ /抽奖名称[:：]\s*.+/
-      errors.add(:base, "缺少奖品") unless self.raw =~ /奖品[:：]\s*.+/
-      errors.add(:base, "缺少获奖人数") unless self.raw =~ /获奖人数[:：]\s*\d+/
-      errors.add(:base, "缺少开奖时间") unless self.raw =~ /开奖时间[:：]\s*[\d\- :]+/
+    def validate(*)
+      super
+      begin
+        if self.post_number == 1
+          tags = self.topic&.tags&.map(&:name) || []
+          is_choujiang = tags.include?("抽奖活动")
+          Rails.logger.warn("choujiang post validate hook: is_first_post=#{self.post_number == 1}, tags=#{tags.inspect}, is_choujiang=#{is_choujiang}")
+          if is_choujiang
+            Rails.logger.warn("choujiang post validate: raw=#{self.raw.inspect}")
+            errors.add(:base, "缺少抽奖名称") unless self.raw =~ /抽奖名称[:：]\s*.+/
+            errors.add(:base, "缺少奖品") unless self.raw =~ /奖品[:：]\s*.+/
+            errors.add(:base, "缺少获奖人数") unless self.raw =~ /获奖人数[:：]\s*\d+/
+            errors.add(:base, "缺少开奖时间") unless self.raw =~ /开奖时间[:：]\s*[\d\- :]+/
+          end
+        end
+      rescue => e
+        Rails.logger.warn("choujiang validate error: #{e.message}")
+      end
     end
   end
 
-  # 只 prepend 一次，防止重复
-  unless Post.included_modules.include?(::ChoujiangPostValidation)
+  unless Post.ancestors.include?(::ChoujiangPostValidation)
     Post.prepend(::ChoujiangPostValidation)
     Rails.logger.warn("choujiang post validation prepended!")
   end
