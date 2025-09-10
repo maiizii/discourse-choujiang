5711e59eb8820c24bc6d4ad2809a69ac75f95c9b
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-10 04:34:44 -0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,9 @@
 # name: discourse-choujiang
 # about: 定时自动开奖的抽奖（choujiang）插件，支持自定义规则与自动开奖
-# version: 0.3
+# version: 0.4
 # authors: maiizii
 # url: https://github.com/maiizii/discourse-choujiang
+# required_plugins: discourse-gamification
 
 enabled_site_setting :choujiang_enabled
 
