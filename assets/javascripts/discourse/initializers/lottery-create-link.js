1b0346642b28f79d17bf1fd8f3f2f42b7e913f27
maiizii <59905570+maiizii@users.noreply.github.com>
2025-09-17 01:29:00 -0700

    Update lottery-create-link.js

--- assets/javascripts/discourse/initializers/lottery-create-link.js
+++ assets/javascripts/discourse/initializers/lottery-create-link.js
@@ -4,15 +4,16 @@ export default {
   name: "lottery-create-link",
   initialize() {
     withPluginApi("1.8.0", (api) => {
-      api.onPageChange(() => {}); // 保证加载
-      // 显示给已登录用户（未登录访问直接输入 URL 也能看到提示）
-      if (!api.getCurrentUser()) return;
+      const currentUser = api.getCurrentUser();
+      if (!currentUser) {
+        return;
+      }
       api.addHeaderDropdownEntry({
         name: "lottery-create",
         displayName: "发布抽奖",
         href: "/lottery/create",
-        icon: "gift"
+        icon: "gift",
       });
     });
-  }
+  },
 };
